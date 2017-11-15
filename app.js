b7ab6129f5dc880b498ebfffafc5166db39d8e36
Lucas Cuba <lucas.cuba4@gmail.com>
2017-11-15 14:34:01 -0500

    initial checkpoint 8 commit

--- app.js
+++ app.js
@@ -1,39 +1,55 @@
 function onReady() {
 
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
+  function createNewToDo() {
 
-    let title = newToDoText.value;
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
 
-    let newLi = document.createElement('li');
-
-    let checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-
-    let removeItemButton = document.createElement('input');
-    removeItemButton.type = "button";
-    removeItemButton.value = "Remove";
-    removeItemButton.id = "removeButton";
-
-    newLi.textContent = title;
-
-    newLi.appendChild(checkbox);
-
-    newLi.appendChild(removeItemButton);
-
-    toDoList.appendChild(newLi);
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
 
     newToDoText.value = '';
 
-    removeItemButton.addEventListener('click', event => {
-      newLi.parentNode.removeChild(newLi);
+    renderTheUI();
+
+  }
+
+  function renderTheUI() {
+
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+
     });
+
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+
+    event.preventDefault();
+    createNewToDo();
+
   });
-}
+
+  renderTheUI();
+
+  }
 
 window.onload = function() {
   onReady();
